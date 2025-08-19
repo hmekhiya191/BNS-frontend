73bb1190a72ad208c883047cc9bc362277c23448
Harsh Mekhiya <harshmekhiya191@gmail.com>
2025-08-19 18:11:43 +0530

    Add Hero component with video background

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -44,7 +44,7 @@ const Hero = () => {
           playsInline
           preload="auto"
         >
-          <source src="/BNS-title.mp4" type="video/mp4" />
+          <source src="" type="video/mp4" />
         </video>
       </div>
 
@@ -59,7 +59,7 @@ const Hero = () => {
         preload="auto"
         style={{ x: smoothX, y: smoothY }}
       >
-        <source src="/BNS-title.mp4" type="video/mp4" />
+        <source src="https://res.cloudinary.com/dhhrwylyd/video/upload/v1755010795/BNS_title_ai6hro.mp4" type="video/mp4" />
       </motion.video>
 
       {/* Centered Text */}
