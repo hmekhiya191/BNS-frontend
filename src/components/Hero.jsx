31449afdcfef9c9d1373feb1d68b8e1378500c7d
Harsh Mekhiya <harshmekhiya191@gmail.com>
2025-08-20 11:30:49 +0530

    Add all project files again

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -44,7 +44,7 @@ const Hero = () => {
           playsInline
           preload="auto"
         >
-          <source src="" type="video/mp4" />
+          <source src="https://res.cloudinary.com/dhhrwylyd/video/upload/v1755010795/BNS_title_ai6hro.mp4" type="video/mp4" />
         </video>
       </div>
 
